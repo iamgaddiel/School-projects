181885f629a6dd326fadd67ec31d0e2b425abe3e
iamgaddiel <ighotagaddiel98@gmail.com>
2022-06-09 07:21:51 -0400

    nade few changes

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import Dashboard, ProfileDetail, Register, profile_update
 
+from post.views import PostFeed
+
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', Dashboard.as_view(), name='dashboard'),
+    path('', PostFeed.as_view(), name='dashboard'),
+    # path('', Dashboard.as_view(), name='dashboard'),
     path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', Register.as_view(), name='register'),
